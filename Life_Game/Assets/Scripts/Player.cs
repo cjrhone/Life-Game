30b98387644b93236f7600add5c94584427ba792
cj <cjrh1films@gmail.com>
2019-01-15 23:10:09 -0800

    fixed aspect ratio

--- Life_Game/Assets/Scripts/Player.cs
+++ Life_Game/Assets/Scripts/Player.cs
@@ -2,29 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
 
-	public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 1f;
+        print("Hello There");
+        
+    }
 
-	// Use this for initialization
-	public void Start () {
-
-		moveSpeed = 2f;
-
-		print ("game has started");
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//transform.Translate(x,y,z)
-		//transform is the Player's transform... we're changing its X position continually with Time.deltaTime
-		transform.Translate(
-			Input.GetAxis("Horizontal")* moveSpeed * Time.deltaTime,
-			Input.GetAxis("Vertical")*Time.deltaTime,
-			0f);
-
-		print(Input.GetAxis("Horizontal"));
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(x,y,z)
+       transform.Translate(
+        Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 
+        Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed, 
+        0f);
+        
+    }
 }
