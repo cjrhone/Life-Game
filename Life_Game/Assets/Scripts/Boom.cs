39c7aeebb0fe64feba03aa00472dd2c49768912f
cj <cjrh1films@gmail.com>
2019-01-17 21:16:01 -0800

    Make retry button inactive, until player object is destroyed

--- Life_Game/Assets/Scripts/Boom.cs
+++ Life_Game/Assets/Scripts/Boom.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Boom : MonoBehaviour
 {
+    //SerialzedField tells Unity to expose only in the Unity editor, but is still private
+    [SerializeField]
+    private GameObject RetryButton;
+
     // Update is called once per frame
     void Update()
     {
@@ -11,12 +15,15 @@ public class Boom : MonoBehaviour
     }
     // used without trigger. Needs rigidbody. 
     void OnCollisionEnter(Collision col) {
-    
+        //null check
+        if(RetryButton != null){
+            RetryButton.SetActive(true);
+        }
         print("OnCollision collision");
         Destroy(col.gameObject);
     }
 
-    // Triggers are like checkpoints and events 
+    // Triggers are checkpoints and events, physically they're usually empty objects 
     //  void OnTriggerEnter(Collider col) {
     
     //     print("OnTrigger collision");
