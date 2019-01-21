fa6a070b00ab0f0a1412e459f894b54be3d1190a
CJ Rhone <charlesrhonejr@gmail.com>
2019-01-20 22:34:37 -0800

    Add AudioManager with functions Play() and Stop()

--- Life_Game/Assets/Scripts/Boom.cs
+++ Life_Game/Assets/Scripts/Boom.cs
@@ -11,16 +11,28 @@ public class Boom : MonoBehaviour
     [SerializeField]
     private GameObject Player;
 
+    [SerializeField]
+    private GameObject Obstacle;
+
     // used without trigger. Needs rigidbody. 
     void OnCollisionEnter(Collision col) {
+
         //null check
         if(RetryButton != null){
             RetryButton.SetActive(true);
         }
+
         print("OnCollision collision");
-        if(Player)
+
+        //tags are easier, because we can give multiple instances the same tags
+        if(col.collider.tag == "Player")
         {
-        print("Destroy Player");
+        print("Destroy... " + col.collider.name);
+      
+        FindObjectOfType<AudioManager>().Stop("Theme");
+
+        FindObjectOfType<AudioManager>().Play("PlayerDeath");
+
         Destroy(Player);
         }
     }
